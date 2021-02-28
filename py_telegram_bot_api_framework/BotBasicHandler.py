17d86e8396cf9b4a0d180d55c5805b3c17efed40
Angelovich <angel777da@gmail.com>
2021-02-28 15:42:16 +0300

    BotBasicHandler extended for inline_query and  callback_query

--- py_telegram_bot_api_framework/BotBasicHandler.py
+++ py_telegram_bot_api_framework/BotBasicHandler.py
@@ -6,7 +6,7 @@ from telegram_bot_api import Update, get_entities_by_type, MessageEntityType, AP
 HandlerFunc = Callable[[Update], bool]
 
 
-class BotCommandHandler(AHandler):
+class BotBasicHandler(AHandler):
 	def __init__(self, api: API, config: dict):
 		self.registered_commands: Dict[str, HandlerFunc] = {}
 		super().__init__(api, config)
@@ -15,6 +15,24 @@ class BotCommandHandler(AHandler):
 		self.registered_commands[cmd] = cmd_handler
 
 	def handle(self, update: Update) -> bool:
+		if update.inline_query and self.handle_inline(update):
+			return True
+
+		if update.callback_query and self.handle_callback(update):
+			return True
+
+		if self.handle_commands(update):
+			return True
+
+		return False
+
+	def handle_inline(self, update: Update) -> bool:
+		return False
+
+	def handle_callback(self, update: Update) -> bool:
+		return False
+
+	def handle_commands(self, update: Update) -> bool:
 		msg = update.message
 		if msg:
 			commands = get_entities_by_type(msg, MessageEntityType.BOT_COMMAND)
@@ -24,5 +42,4 @@ class BotCommandHandler(AHandler):
 				cmd_handler = self.registered_commands.get(cmd, False)
 				if cmd_handler:
 					return cmd_handler(update)
-
 		return False
