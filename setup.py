3b1930adf6b802c5484d9897156193317972982f
Angelovich <angel777da@gmail.com>
2021-06-13 10:29:06 +0300

    pypi configure

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="py-telegram-bot-api-framework",
-    version="0.0.1",
+    version="0.0.2",
     description="Telegram bot framework over py-telegram-bot-api",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,5 +25,5 @@ setup(
     ],
     packages=["py_telegram_bot_api_framework", ],
     include_package_data=True,
-    install_requires=[],
+    install_requires=["py_telegram_bot_api", ],
 )
